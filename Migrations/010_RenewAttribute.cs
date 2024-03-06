8e6bde921887ecf61e466c0dcd601021c8154854
Mark Lee <mark@9embers.com>
2024-03-06 13:39:20 -0500

    Fixed issue where the wrong category was used

--- Migrations/010_RenewAttribute.cs
+++ Migrations/010_RenewAttribute.cs
@@ -9,7 +9,9 @@ namespace CIAResearch.Migrations
     {
         public override void Up()
         {
-            RockMigrationHelper.UpdatePersonAttribute( Rock.SystemGuid.FieldType.INTEGER, "6F8A431C-BEBD-4D33-AAD6-1D70870329C2", "Auto Renewal Days", "AutoRenewalDays", "", "The number of days to wait before auto renewing a background check.", 0, "", "d0852aff-8dd8-4f44-8218-d1342a39ff64" );
+            RockMigrationHelper.UpdateCategory( Rock.SystemGuid.EntityType.ATTRIBUTE, "Safety & Security", "fa fa-medkit", "Information related to safety and security of organization", "4D1E1EBA-ABF2-4A7C-8ADF-65CB5AAE94E2" );
+
+            RockMigrationHelper.UpdatePersonAttribute( Rock.SystemGuid.FieldType.INTEGER, "4D1E1EBA-ABF2-4A7C-8ADF-65CB5AAE94E2", "Auto Renewal Days", "AutoRenewalDays", "", "The number of days to wait before auto renewing a background check.", 0, "", "d0852aff-8dd8-4f44-8218-d1342a39ff64" );
         }
 
         public override void Down()
